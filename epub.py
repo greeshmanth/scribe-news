a3b0f1ab605c778a453e173d9b078b33b32406cb
Greeshmanth Koganti <greeshmnth@weiko.org>
2025-04-10 14:30:15 +0200

    Inital commit

---
+++ epub.py
@@ -0,0 +1,82 @@
+from ebooklib import epub
+import newspaper
+import requests
+from bs4 import BeautifulSoup
+import utils
+from datetime import date
+
+# Step 1: Fetch the RSS feed
+#rss_url = ""  # Replace with your RSS feed URL
+rss_urls = [
+    ("Detusche Welle","https://rss.dw.com/rdf/rss-en-top"),
+    ("BBC","https://feeds.bbci.co.uk/news/world/rss.xml"),
+    ("The Guardian", "https://www.theguardian.com/world/rss"),
+    ("The New Yorker","https://www.newyorker.com/feed/everything"),
+    ("Die Zeit","https://newsfeed.zeit.de/english/index")
+]
+
+urls = utils.get_articles_from_feeds(rss_urls,10)
+utils.get_new_cover_image()
+utils.edit_cover()
+
+# Create a new book
+book = epub.EpubBook()
+
+# Set metadata
+book.set_title("News - " +  date.today().strftime("%B %d, %Y"))
+book.set_language("en")
+book.add_author("Greeshmanth Koganti")
+book.toc = []
+book.spine = ["nav"]
+
+# Add cover image
+with open("cover.jpg", "rb") as cover_file:
+    cover = cover_file.read()
+book.set_cover("cover.jpg", cover)
+
+for source, url_list in urls:
+
+    chapters = []
+
+    for index, url in enumerate(url_list):
+
+        article = newspaper.article(url)
+        title = article.title.split(" – DW")[0]
+        text = article.text.replace("\n", "<br>")
+
+        print(title)
+
+        chapter = epub.EpubHtml(title=title, file_name=source+str(index)+".xhtml", lang="en")
+        chapter.content = "<h1>" + title + "</h1><p>" + text + "</p>"
+
+        # Add chapter to book
+        book.add_item(chapter)
+
+        chapters.append(chapter)
+        book.spine.append(chapter)
+
+    # Define Table of Contents explicitly
+    book.toc.append((epub.Section(source), chapters))
+    
+# Add navigation files
+book.add_item(epub.EpubNcx())
+book.add_item(epub.EpubNav())
+
+# Save the book as an EPUB file
+epub.write_epub("news.epub", book, {})
+
+print("EPUB created successfully!")
+
+utils.send_mail_to_kindle("news.epub")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,102 @@
+import requests
+from bs4 import BeautifulSoup
+import resend
+import base64
+from PIL import Image, ImageDraw, ImageFont
+from datetime import date
+
+def get_articles_from_feeds(rss_urls, count = 10):
+    urls = []
+
+    for source, rss_url in rss_urls:
+        response = requests.get(rss_url)
+
+        # Step 2: Parse the feed using Beautiful Soup
+        soup = BeautifulSoup(response.content, 'xml')  # Use 'xml' parser for RSS feeds
+
+        # Step 3: Extract items from the feed
+        items = soup.find_all('item')  # Find all <item> tags
+
+
+        loop_urls = []
+        # Step 4: Iterate through the items and print details
+        for item in items[:count]:
+            loop_urls.append(item.find('link').text)  # Extract <link> content
+
+        urls.append((source, loop_urls))    
+
+    return urls
+
+def get_new_cover_image(filename="cover.jpg"):
+    # Send a request to the URL
+    response = requests.get("https://picsum.photos/1600/2560/?grayscale")
+
+    # Save the image
+    with open("cover.jpg", "wb") as file:
+        file.write(response.content)
+
+    print("Image downloaded successfully!")
+
+def send_mail_to_kindle(file_path = "news.epub"):
+
+    resend.api_key = "re_PYuK9JVV_425azDhwRKa5cMDGWqXPPpfW"
+
+    # Read the file and encode it in Base64
+    with open(file_path, "rb") as f:
+        attachment_content = base64.b64encode(f.read()).decode("utf-8")  # Proper encoding
+
+    params: resend.Emails.SendParams = {
+        "from": "Greeshmanth Koganti <greeshmanth@weiko.org>",
+        "to": ["rg_OtPfEh@kindle.com"],
+        #"to": ["greeshmanth@weiko.org"],
+        "subject": "News",
+        "html": "News",
+        "attachments" : [
+            {
+                "content": attachment_content,
+                "filename": file_path,
+            }
+        ]
+    }
+
+    email = resend.Emails.send(params)
+    print(email)
+
+def edit_cover(filename="cover.jpg"):
+    # Load the image
+    image = Image.open(filename)
+
+    # Create a drawing context
+    draw = ImageDraw.Draw(image)
+
+    # ...
+    # Define the text properties
+    font = ImageFont.truetype("Coolvetica Rg.otf", 144)
+    position = (50, 50)
+
+    # Define text
+    text = date.today().strftime("%B %d")
+
+    # Get text bounding box (to calculate text dimensions)
+    bbox = draw.textbbox((0, 0), text, font=font)
+    text_width = bbox[2] - bbox[0]  # Width
+    text_height = bbox[3] - bbox[1]  # Height
+
+    # Calculate centered position
+    image_width, image_height = (1600, 2560)
+    x = (image_width - text_width) // 2
+    y = (image_height - text_height) // 2
+
+    # Define the box position and color
+    white_color = (255, 255, 255)  # RGB for white
+    box_position = [(0, 0), (1600, text_height*2)]  # Top-left and bottom-right corners
+
+    # Draw the rectangle (white box)
+    transparent_color = (0, 0, 0, 100)  # Black with 100/255 transparency (adjust as needed)
+    draw.rectangle(box_position, fill=255)
+
+    # Draw text at the centered position
+    draw.text((x, 50), text, font=font, fill='black')
+
+    # Save or display the modified image
+    image.save(filename)
